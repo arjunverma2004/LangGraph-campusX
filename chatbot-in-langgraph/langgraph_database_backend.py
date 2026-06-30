f10a36d7a72580860feffbc9023d69ca2f18b8cf
arjunverma2004 <arjuncoding2004@gmail.com>
2026-06-30 22:34:06 +0530

    yeahyeah

--- chatbot-in-langgraph/langgraph_database_backend.py
+++ chatbot-in-langgraph/langgraph_database_backend.py
@@ -19,7 +19,12 @@ def chat_node(state: ChatState):
     response = llm.invoke(messages)
     return {"messages": [response]}
 
+#First we need to create a connection to the SQLite database, if db doesn't exist it will be created. We will use the sqlite3 library to do this. We will also create a checkpointer that will save the state of the chatbot to the database.
+
 conn = sqlite3.connect(database='chatbot.db', check_same_thread=False)
+
+#we are using check_same_thread=False because we will be using multiple threads, so it will stop checking if convo is same thread as of db or not.
+
 # Checkpointer
 checkpointer = SqliteSaver(conn=conn)
 
@@ -33,6 +38,7 @@ chatbot = graph.compile(checkpointer=checkpointer)
 def retrieve_all_threads():
     all_threads = set()
     for checkpoint in checkpointer.list(None):
+        #checkpointer.list(None) returns a list of all checkpoints in the database, we will iterate through them and add the thread_id to the set of all_threads.
         all_threads.add(checkpoint.config['configurable']['thread_id'])
 
     return list(all_threads)
